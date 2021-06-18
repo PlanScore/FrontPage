dc17fb1c76b7d96605c1df7b1dfa7e9a3aabea6c
Michal Migurski <mike@teczno.com>
2021-06-17 21:51:29 -0700

    Copied Friends & Resources page content

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,7 @@ const GLOBAL_JS6_FILES = [
     './patternlibrary_htmltemplate/index.js6',
     './about/index.js6',
     './about/historical-data/index.js6',
+    './about/friends-resources/index.js6',
     './metrics/index.js6',
     './metrics/efficiencygap/index.js6',
     './metrics/meanmedian/index.js6',
