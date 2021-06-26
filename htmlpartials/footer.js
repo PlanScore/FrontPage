d4a04051197e761849c0c2edad57391140ecce8b
Paul Irish <commits@paul.irish>
2021-06-26 16:00:39 -0700

    center the footer

--- htmlpartials/footer.js
+++ htmlpartials/footer.js
@@ -1,16 +1,17 @@
 const htmlblock = `
-    <div class="container-fluid footer">
+    <div class="footer">
+    <div class="container">
     <div class="row">
-        <p class="col-md-2">
+        <p class="col-md-3">
             <a target="_blank" href="mailto:info@planscore.org"><img src="https://planscore.org/images/email-logo.svg"> info@planscore.org</a>
         </p>
-        <p class="col-md-2">
+        <p class="col-md-3">
             <a target="_blank" href="https://twitter.com/PlanScore"><img src="https://planscore.org/images/twitter-logo.svg"> @PlanScore</a>
         </p>
-        <p class="col-md-2">
+        <p class="col-md-3">
             <a target="_blank" href="https://github.com/PlanScore/PlanScore"><img src="https://planscore.org/images/github-logo.svg"> Github</a>
         </p>
-        <form class="col-md-2" action="https://www.paypal.com/donate" method="post" target="_top">
+        <form class="col-md-3" action="https://www.paypal.com/donate" method="post" target="_top">
             <input type="hidden" name="hosted_button_id" value="C9G45F294EKEG" />
             <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
             <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
@@ -20,6 +21,7 @@ const htmlblock = `
         <span class="col-md-12">
             PlanScore is a 501(c)(3) non-profit organization, EIN 83-1367310
         </span>
+    </div>
     </div>
     </div>
 
