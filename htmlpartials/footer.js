4fa6f6b198a6c8eb7d1c60bcfbb0b63e83cc7795
Paul Irish <commits@paul.irish>
2021-06-26 16:17:02 -0700

    footer items: go from 4x1 to 2x2 on mobile.

--- htmlpartials/footer.js
+++ htmlpartials/footer.js
@@ -2,23 +2,23 @@ const htmlblock = `
     <div class="footer">
     <div class="container">
     <div class="row">
-        <p class="col-md-3">
+        <p class="col-sm-3 col-xs-6">
             <a target="_blank" href="mailto:info@planscore.org"><img src="https://planscore.org/images/email-logo.svg"> info@planscore.org</a>
         </p>
-        <p class="col-md-3">
+        <p class="col-sm-3 col-xs-6">
             <a target="_blank" href="https://twitter.com/PlanScore"><img src="https://planscore.org/images/twitter-logo.svg"> @PlanScore</a>
         </p>
-        <p class="col-md-3">
+        <p class="col-sm-3 col-xs-6">
             <a target="_blank" href="https://github.com/PlanScore/PlanScore"><img src="https://planscore.org/images/github-logo.svg"> Github</a>
         </p>
-        <form class="col-md-3" action="https://www.paypal.com/donate" method="post" target="_top">
+        <form class="col-sm-3 col-xs-6" action="https://www.paypal.com/donate" method="post" target="_top">
             <input type="hidden" name="hosted_button_id" value="C9G45F294EKEG" />
             <input type="submit" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" class="btn btn-primary" value="Donate" style="margin: 0;">
             <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
         </form>
     </div>
     <div class="row">
-        <span class="col-md-12">
+        <span class="col-sm-12">
             PlanScore is a 501(c)(3) non-profit organization, EIN 83-1367310
         </span>
     </div>
