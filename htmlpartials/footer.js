2c0042ad6e0b1afdf961c0da2ce2242339e19c1c
spncr <spncr@thinkshout.com>
2021-06-30 11:50:58 -0700

    Footer desktop redesign WIP

--- htmlpartials/footer.js
+++ htmlpartials/footer.js
@@ -1,37 +1,36 @@
 const htmlblock = `
-    <div class="footer">
-    <div class="container">
-    <div class="row">
-        <p class="col-sm-3 col-xs-6">
-            <a target="_blank" href="mailto:info@planscore.org"><img src="https://planscore.org/images/email-logo.svg"> info@planscore.org</a>
-        </p>
-        <p class="col-sm-3 col-xs-6">
-            <a target="_blank" href="https://twitter.com/PlanScore"><img src="https://planscore.org/images/twitter-logo.svg"> @PlanScore</a>
-        </p>
-        <p class="col-sm-3 col-xs-6">
-            <a target="_blank" href="https://github.com/PlanScore/PlanScore"><img src="https://planscore.org/images/github-logo.svg"> Github</a>
-        </p>
-        <form class="col-sm-3 col-xs-6" action="https://www.paypal.com/donate" method="post" target="_top">
-            <input type="hidden" name="hosted_button_id" value="C9G45F294EKEG" />
-            <input type="submit" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" class="btn btn-primary" value="Donate" style="margin: 0;">
-            <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
-        </form>
+  <div class="footer">
+    <div class="callout">
+      <a href="https://campaignlegal.org" target="_blank" title="Visit Campaign Legal Center">
+        <img src="/images/CLC-logo.svg" alt="Campaign Legal Center Logo" />
+      </a>
+      <p>
+        PlanScore is a project of Campaign Legal Center.
+      </p>
     </div>
-    <div class="row">
-        <span class="col-sm-12">
-            PlanScore is a 501(c)(3) non-profit organization, EIN 83-1367310
-        </span>
+    <div class="links">
+      <a target="_blank" href="mailto:info@planscore.org" title="Email PlanScore">
+        <img src="https://planscore.org/images/email-logo.svg" alt="email icon">
+      </a>
+      <a target="_blank" href="https://twitter.com/PlanScore" title="PlanScore on Twitter">
+        <img src="https://planscore.org/images/twitter-logo.svg" alt="Twitter icon">
+      </a>
+      <a target="_blank" href="https://github.com/PlanScore/PlanScore" title="PlanScore on GitHub">
+        <img src="https://planscore.org/images/github-logo.svg" alt="GitHub icon">
+      </a>
+      <div class="legal">
+        PlanScore is a 501(c)(3) non-profit organization, EIN 83-1367310
+      </div>
     </div>
-    </div>
-    </div>
+  </div>
 
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-65629552-4"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-65629552-4"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
 
-      gtag('config', 'UA-65629552-4');
-    </script>
-    `;
+    gtag('config', 'UA-65629552-4');
+  </script>
+  `;
 module.exports = htmlblock;
