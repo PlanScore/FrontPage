2c5b651bc4c4e267791376081f1b7168b421971b
Michal Migurski <mike@teczno.com>
2022-10-07 22:43:50 -0700

    Updated additional mentions of CLC

--- htmlpartials/navbar.js
+++ htmlpartials/navbar.js
@@ -8,7 +8,7 @@ const htmlblock = `
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="/"><img id="brand-logo" src="/images/logo.svg" alt="PlanScore: a project of Campaign Legal Center"/></a>
+        <a class="navbar-brand" href="/"><img id="brand-logo" src="/images/logo.svg" alt="PlanScore"/></a>
     </div>
     <div class="collapse navbar-collapse" id="navbar">
         <ul class="nav navbar-nav navbar-right">
